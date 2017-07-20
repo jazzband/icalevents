7d4540b1eed58cdf4e81601440651ed4d2f018f0
Thomas Irgang <thomas@irgang-la.de>
2017-07-20 09:04:05 +0200

    test module for icalevents added

--- test.py
+++ test.py
@@ -2,5 +2,6 @@ import unittest
 
 from test.test_icaldownload import *
 from test.test_icalparser import *
+from test.test_icalevents import *
 
 unittest.main()
---
+++ test/test_icalevents.py
@@ -0,0 +1,8 @@
+import unittest
+from icalevents import *
+
+
+class ICalEventsTests(unittest.TestCase):
+
+    def test_something(self):
+        self.assertEqual(True, False)
