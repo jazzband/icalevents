1d0df6468f1d0492cafc0541b2caed6922b915cc
Irgang <Thomas.Irgang@elektrobit.com>
2017-07-20 18:02:46 +0200

    code coverage extended

--- test/test_icalevents.py
+++ test/test_icalevents.py
@@ -5,6 +5,22 @@ from time import sleep
 
 
 class ICalEventsTests(unittest.TestCase):
+    def test_events_url(self):
+        url = "https://raw.githubusercontent.com/irgangla/icalevents/master/test/test_data/basic.ics"
+        start = date(2017, 5, 18)
+        end = date(2017, 5, 19)
+
+        evs = icalevents.events(url=url, file=None, start=start, end=end)
+
+        self.assertEqual(len(evs), 2, "two events are found")
+
+    def test_events_start(self):
+        ical = "test/test_data/basic.ics"
+        start = date(2017, 5, 16)
+
+        evs = icalevents.events(url=None, file=ical, start=start)
+
+        self.assertEqual(len(evs), 3, "three events are found")
 
     def test_events(self):
         ical = "test/test_data/basic.ics"
@@ -14,6 +30,19 @@ class ICalEventsTests(unittest.TestCase):
         evs = icalevents.events(url=None, file=ical, start=start, end=end)
 
         self.assertEqual(len(evs), 2, "two events are found")
+
+    def test_events_async_url(self):
+        url = "https://raw.githubusercontent.com/irgangla/icalevents/master/test/test_data/basic.ics"
+        start = date(2017, 5, 18)
+        end = date(2017, 5, 19)
+        key = "basic"
+
+        icalevents.events_async(key, url=url, file=None, start=start, end=end)
+
+        sleep(4)
+
+        self.assertTrue(icalevents.all_done(key), "request is finished")
+        self.assertEqual(len(icalevents.latest_events(key)), 2, "two events are found")
 
     def test_events_async(self):
         ical = "test/test_data/basic.ics"
