f04272f840c2785556e06bccb2bb2104acd018e2
Irgang <Thomas.Irgang@elektrobit.com>
2017-07-21 07:38:17 +0200

    auto deploy, v0.1.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
         "pytz",
         "datetime",
     ],
-    version = '0.1.0',
+    version = '0.1.1',
     description = 'iCal downloader and parser',
     author = 'Thomas Irgang',
     author_email = 'thomas@irgang-la.de',
