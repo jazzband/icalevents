8572fafa15eb3fff4a1ae1971205d98f0a910bc4
dlichtistw <dlichtistw@gmx.de>
2018-09-12 06:53:27 +0200

    Description on recurring events
    Copy description when creating repeated copies of recurring event.

--- icalevents/icalparser.py
+++ icalevents/icalparser.py
@@ -33,6 +33,7 @@ class Event:
         """
         self.uid = -1
         self.summary = None
+        self.description = None
         self.start = None
         self.end = None
         self.all_day = True
@@ -103,6 +104,7 @@ class Event:
 
         ne = Event()
         ne.summary = self.summary
+        ne.description = self.description
         ne.start = new_start
         ne.end = (new_start + duration)
         ne.all_day = (self.all_day and (new_start - self.start).seconds == 0)
