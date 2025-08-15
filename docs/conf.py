eacbe3dad4eabc78902b0349aac97f2822b0d30c
Martin Eigenmann <github@eigenmannmartin.ch>
2025-08-15 12:24:22 +0200

    ci: release 0.3.0

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2024, jazzband"
 author = "Martin Eigenmann"
 
 # The full version, including alpha/beta/rc tags
-release = "0.2.1"
+release = "0.3.0"
 
 
 # -- General configuration ---------------------------------------------------
